77ad082937479cd494e33cec4f9b661a4fe4b42e
Egorishche7 <95584293+Egorishche7@users.noreply.github.com>
2023-04-12 12:56:14 +0300

    Add files via upload

---
+++ Lab2/Child.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        cerr << "Error!" << endl;
+        exit(1);
+    }
+
+    int size = atoi(argv[1]);
+    cout << "Source array:" << endl;
+    for (int i = 0; i < size; i++)
+        cout << "array[" << i + 1 << "]=" << atof(argv[i + 2]) << endl;
+
+    double* newArr = new double[size];
+    for (int i = 0; i < size; i++)
+        newArr[i] = atof(argv[i + 2]);
+
+    double temp;
+    for (int i = 0; i < size - 1; i++)
+        for (int j = 0; j < size - i - 1; j++)
+            if (newArr[j] > newArr[j + 1])
+            {
+                temp = newArr[j];
+                newArr[j] = newArr[j + 1];
+                newArr[j + 1] = temp;
+            }
+
+    cout << "\nSorted array:" << endl;
+    for (int i = 0; i < size; i++)
+        cout << "array[" << i + 1 << "]=" << newArr[i] << endl;
+
+    delete[] newArr;
+    newArr = nullptr;
+
+    system("pause");
+
+    return 0;
+}
---
+++ Lab2/Parent.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.dwFlags = STARTF_USECOUNTCHARS;
+    si.dwYCountChars = 600;
+
+    int size;
+    cout << "Input array size: ";
+    cin >> size;
+    double* arr = new double[size];
+
+    cout << "Input array elements:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "array[" << i + 1 << "]=";
+        cin >> arr[i];
+    }
+
+    wstring line1 = L"\"Child.exe\" ";
+    line1 += to_wstring(size) + L" ";
+    for (int i = 0; i < size; i++)
+        line1 += to_wstring(arr[i]) + L" ";
+
+    wchar_t* line2 = new wchar_t[line1.size()];
+    wcscpy_s(line2, line1.size() + 1, line1.c_str());
+
+    if (!CreateProcess(NULL, line2, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+    {
+        cerr << "\nProcess is not created.";
+        return GetLastError();
+    }
+    else
+    {
+        cout << "\nProcess is created." << endl;
+        WaitForSingleObject(pi.hProcess, INFINITE);
+    }
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    delete[] arr, line2;
+    arr = nullptr;
+    line2 = nullptr;
+
+    return 0;
+}
