edbeed1d3cc46bf6ba8de2b7309b89d24c200ddf
Egorishche7 <95584293+Egorishche7@users.noreply.github.com>
2023-05-05 08:17:40 +0300

    Add files via upload

---
+++ Lab5/Server.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int info[2];
+	cout << "Enter array size: ";
+	cin >> info[0];
+
+	double* arr = new double[info[0]];
+	srand(time(0));
+	for (int i = 0; i < info[0]; i++)
+		arr[i] = ((double)rand() / RAND_MAX) * 20;
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	HANDLE hEvent;
+	HANDLE hReadPipe, hWritePipe;
+	SECURITY_ATTRIBUTES sa;
+
+	hEvent = CreateEvent(nullptr, FALSE, FALSE, L"Event");
+	if (hEvent == nullptr)
+	{
+		cout << "Server: Event wasn't created.";
+		return GetLastError();
+	}
+
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	sa.lpSecurityDescriptor = nullptr;
+	sa.bInheritHandle = TRUE;
+
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, 0))
+	{
+		cout << "Server: Pipe creation failed.";
+		return GetLastError();
+	}
+
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	wchar_t* line = new wchar_t[80];
+	wsprintf(line, L"\"Sum.exe\" %d %d", (int)hReadPipe, (int)hWritePipe);
+	if (!CreateProcess(nullptr, line, nullptr, nullptr, TRUE, CREATE_NEW_CONSOLE, FALSE, FALSE, &si, &pi))
+	{
+		cout << "Server: Client creation failed.";
+		return GetLastError();
+	}
+
+	cout << "Enter number N: ";
+	cin >> info[1];
+
+	DWORD dwBytesWritten;
+	if (!WriteFile(hWritePipe, info, sizeof(int) * 2, &dwBytesWritten, nullptr))
+	{
+		cout << "Server: Info wasn't written.";
+		return GetLastError();
+	}
+	if (!WriteFile(hWritePipe, arr, sizeof(double) * info[0], &dwBytesWritten, nullptr))
+	{
+		cout << "Server: Array elements weren't written.";
+		return GetLastError();
+	}
+
+	WaitForSingleObject(hEvent, INFINITE);
+
+	int m;
+	DWORD dwBytesRead;
+	if (!ReadFile(hReadPipe, &m, sizeof(int), &dwBytesRead, nullptr))
+	{
+		cout << "Server: Squares array size wasn't read.";
+		return GetLastError();
+	}
+	double* squares = new double[m];
+	if (!ReadFile(hReadPipe, squares, sizeof(double) * m, &dwBytesRead, nullptr))
+	{
+		cout << "Server: Squares weren't read.";
+		return GetLastError();
+	}
+	double sum;
+	if (!ReadFile(hReadPipe, &sum, sizeof(double), &dwBytesRead, nullptr))
+	{
+		cout << "Server: Sum wasn't read.";
+		return GetLastError();
+	}
+
+	cout << "\nArray size: " << info[0] << endl;
+	cout << "N = " << info[1] << endl;
+	cout << "Source array:" << endl;
+	for (int i = 0; i < info[0]; i++)
+		cout << "array[" << i + 1 << "]=" << arr[i] << endl;
+	cout << "\nNew array (squares of array elements > N):" << endl;
+	for (int i = 0; i < m; i++)
+		cout<< "array[" << i + 1 << "]=" << squares[i] << endl;
+	cout << "Sum of squares: " << sum << endl;
+
+	CloseHandle(hReadPipe);
+	CloseHandle(hWritePipe);
+	CloseHandle(hEvent);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	delete[] arr, line, squares;
+	arr = squares = nullptr;
+	line = nullptr;
+
+	return 0;
+}
---
+++ Lab5/Sum.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	if (argc == 1)
+	{
+		cerr << "Error";
+		return 1;
+	}
+
+	HANDLE hReadPipe = (HANDLE)atoi(argv[1]);
+	HANDLE hWritePipe = (HANDLE)atoi(argv[2]);
+	HANDLE hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Event");
+
+	if (hEvent == nullptr)
+	{
+		cout << "Client: Event wasn't read.";
+		return GetLastError();
+	}
+
+	int info[2];
+	DWORD dwBytesRead;
+	if (!ReadFile(hReadPipe, info, sizeof(int) * 2, &dwBytesRead, nullptr))
+		cout << "Client: Info wasn't read.";
+
+	int n = info[0], N = info[1];
+
+	double* arr = new double[n];
+	if (!ReadFile(hReadPipe, arr, sizeof(double) * n, &dwBytesRead, nullptr))
+		cout << "Client: Array elements weren't read.";
+
+	int m = 0;
+	cout << "Array elements > N:" << endl;
+	for (int i = 0; i < n; i++)
+		if (arr[i] > N)
+		{
+			cout << arr[i] << endl;
+			m++;
+		}
+
+	cout << "New array (squares of array elements > N):" << endl;
+	double* squares = new double[m];
+	double sum = 0;
+	for (int i = 0, j = 0; i < n; i++)
+		if (arr[i] > N)
+		{
+			squares[j] = arr[i] * arr[i];
+			cout << "array[" << j + 1 << "]=" << squares[j] << endl;
+			sum += squares[j];
+			j++;
+		}
+
+	DWORD dwBytesWritten;
+	if (!WriteFile(hWritePipe, &m, sizeof(m), &dwBytesWritten, nullptr))
+		cout << "Client: Squares array size wasn't written.";
+	if (!WriteFile(hWritePipe, squares, sizeof(double) * m, &dwBytesWritten, nullptr))
+		cout << "Client: Squares wasn't written.";
+	if (!WriteFile(hWritePipe, &sum, sizeof(sum), &dwBytesWritten, nullptr))
+		cout << "Client: Sum wasn't written.";
+
+	cout << "Sum of squares: " << sum << endl;
+
+	SetEvent(hEvent);
+	CloseHandle(hReadPipe);
+	CloseHandle(hWritePipe);
+	CloseHandle(hEvent);
+
+	delete[] arr, squares;
+	arr = squares = nullptr;
+
+	system("pause");
+
+	return 0;
+}
