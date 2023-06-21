2916402e2ee219cc3b181d8381194ea6963b83b6
Egor Gulin <95584293+Egorishche7@users.noreply.github.com>
2023-06-21 04:34:57 +0300

    Update Reader.cpp

--- Lab4/Reader.cpp
+++ Lab4/Reader.cpp
@@ -1,81 +1,96 @@
-﻿#include <iostream>
+#include <iostream>
 #include <Windows.h>
 
-using namespace std;
+const int ARRAY_SIZE = 3;
+const int A_INDEX = 0;
+const int B_INDEX = 1;
+const int C_INDEX = 2;
+const int NUM_OF_MESSAGES_INDEX = 1;
+const int SEMAPHORE_DECREASE_STEP = 1;
 
-int main(int args, char** argv)
-{
-	HANDLE Semaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"Semaphore");
-	if (Semaphore == nullptr)
-	{
-		cout << "Reader: Semaphore opening failed\n";
+HANDLE semaphore;
+HANDLE* eventsReader;
+HANDLE eventReaderEnded;
+
+int openEvents() {
+	eventsReader = new HANDLE[ARRAY_SIZE];
+	eventsReader[A_INDEX] = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"readerA");
+	if (nullptr == eventsReader[A_INDEX]) {
+		std::cout << "Reader: Event_A opening failed\n";
+		system("pause");
+		return GetLastError();
+	}
+	eventsReader[B_INDEX] = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"readerB");
+	if (nullptr == eventsReader[B_INDEX]) {
+		std::cout << "Reader: Event_B opening failed\n";
+		system("pause");
+		return GetLastError();
+	}
+	eventsReader[C_INDEX] = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"readerC");
+	if (nullptr == eventsReader[C_INDEX]) {
+		std::cout << "Reader: Event_C opening failed\n";
 		system("pause");
 		return GetLastError();
 	}
 
-	WaitForSingleObject(Semaphore, INFINITE);
-	cout << "Reader: Active process\n";
-
-	HANDLE* Events_Reader = new HANDLE[3];
-	Events_Reader[0] = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Reader_A");
-	if (Events_Reader[0] == nullptr)
-	{
-		cout << "Reader: Event_A opening failed\n";
-		system("pause");
-		return GetLastError();
-	}
-	Events_Reader[1] = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Reader_B");
-	if (Events_Reader[1] == nullptr)
-	{
-		cout << "Reader: Event_B opening failed\n";
-		system("pause");
-		return GetLastError();
-	}
-	Events_Reader[2] = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Reader_C");
-	if (Events_Reader[2] == nullptr)
-	{
-		cout << "Reader: Event_C opening failed\n";
+	eventReaderEnded = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"readerEnded");
+	if (nullptr == eventReaderEnded) {
+		std::cout << "Writer: End opening failed\n";
 		system("pause");
 		return GetLastError();
 	}
 
-	HANDLE Event_Reader_Ended = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Reader_Ended");
-	if (Event_Reader_Ended == nullptr)
-	{
-		cout << "Writer: End opening failed\n";
-		system("pause");
-		return GetLastError();
-	}
+	return 0;
+}
 
-	for (int i = 0; i < atoi(argv[1]); i++)
-	{
-		int k = WaitForMultipleObjects(3, Events_Reader, FALSE, INFINITE);
+void startMessageReading(int m) {
+	for (int i = 0; i < m; i++) {
+		int k = WaitForMultipleObjects(ARRAY_SIZE, eventsReader, FALSE, INFINITE);
 		k -= WAIT_OBJECT_0;
-		if (k == 0)
-		{
-			cout << "Reader: A\n";
-			ResetEvent(Events_Reader[0]);
-		}
-		else if (k == 1)
-		{
-			cout << "Reader: B\n";
-			ResetEvent(Events_Reader[1]);
-		}
-		else
-		{
-			cout << "Reader: C\n";
-			ResetEvent(Events_Reader[2]);
+		if (k == A_INDEX) {
+			std::cout << "Reader: A\n";
+		} else if (k == B_INDEX) {
+			std::cout << "Reader: B\n";
+		} else {
+			std::cout << "Reader: C\n";
 		}
 	}
+}
 
-	ReleaseSemaphore(Semaphore, 1, nullptr);
-	SetEvent(Event_Reader_Ended);
+void releaseResources() {
+	CloseHandle(semaphore);
+	CloseHandle(eventsReader[A_INDEX]);
+	CloseHandle(eventsReader[B_INDEX]);
+	CloseHandle(eventsReader[C_INDEX]);
+	CloseHandle(eventReaderEnded);
 
-	CloseHandle(Semaphore);
-	CloseHandle(Events_Reader[0]);
-	CloseHandle(Events_Reader[1]);
-	CloseHandle(Events_Reader[2]);
-	CloseHandle(Event_Reader_Ended);
+	delete[] eventsReader;
+	eventsReader = nullptr;
+}
+
+int main(int args, char* argv[]) {
+
+	semaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"semaphore");
+	if (semaphore == nullptr) {
+		std::cout << "Reader: Semaphore opening failed\n";
+		system("pause");
+		return GetLastError();
+	}
+
+	WaitForSingleObject(semaphore, INFINITE);
+	std::cout << "Reader: Active process\n";
+
+	int flag = openEvents();
+	if (flag != 0) {
+		return flag;
+	}
+
+	startMessageReading(atoi(argv[NUM_OF_MESSAGES_INDEX]));
+
+	ReleaseSemaphore(semaphore, SEMAPHORE_DECREASE_STEP, nullptr);
+	SetEvent(eventReaderEnded);
+
+	releaseResources();
 	system("pause");
 
 	return 0;
