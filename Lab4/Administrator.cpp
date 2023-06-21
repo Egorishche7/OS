c25721711afa926c3a48a433267236854c161677
Egor Gulin <95584293+Egorishche7@users.noreply.github.com>
2023-06-21 04:34:09 +0300

    Update Administrator.cpp

--- Lab4/Administrator.cpp
+++ Lab4/Administrator.cpp
@@ -1,105 +1,155 @@
-﻿#include <iostream>
+#include <iostream>
 #include <Windows.h>
 #include <string>
 
-using namespace std;
+const int ARRAY_SIZE = 3;
+const int A_INDEX = 0;
+const int B_INDEX = 1;
+const int C_INDEX = 2;
+const int MIN_SEMAPHORE_VALUE = 1;
+const int MAX_SEMAPHORE_VALUE = 1;
+const int MIN_NUMBER_OF_PROCESSES = 1;
+const int MIN_NUMBER_OF_MESSAGES = 1;
+const int OFFSET = 1;
 
-int main()
-{
-	HANDLE Mutex = CreateMutex(nullptr, FALSE, L"Mutex");
+STARTUPINFO* siW;
+STARTUPINFO* siR;
+PROCESS_INFORMATION* piW;
+PROCESS_INFORMATION* piR;
 
-	HANDLE Semaphore = CreateSemaphore(nullptr, 1, 1, L"Semaphore");
+HANDLE mutex;
+HANDLE semaphore;
+HANDLE eventReaderA;
+HANDLE eventReaderB;
+HANDLE eventReaderC;
+HANDLE* eventsWriter;
+HANDLE eventWriterEnded;
+HANDLE eventReaderEnded;
 
-	HANDLE Event_Reader_A = CreateEvent(nullptr, FALSE, FALSE, L"Reader_A");
-	HANDLE Event_Reader_B = CreateEvent(nullptr, FALSE, FALSE, L"Reader_B");
-	HANDLE Event_Reader_C = CreateEvent(nullptr, FALSE, FALSE, L"Reader_C");
-	HANDLE* Events_Writer = new HANDLE[3];
-	Events_Writer[0] = CreateEvent(nullptr, FALSE, FALSE, L"Writer_A");
-	Events_Writer[1] = CreateEvent(nullptr, FALSE, FALSE, L"Writer_B");
-	Events_Writer[2] = CreateEvent(nullptr, FALSE, FALSE, L"Writer_C");
-	HANDLE Event_Writer_Ended = CreateEvent(nullptr, FALSE, FALSE, L"Writer_Ended");
-	HANDLE Event_Reader_Ended = CreateEvent(nullptr, FALSE, FALSE, L"Reader_Ended");
+void createSynchObj() {
+	mutex = CreateMutex(nullptr, FALSE, L"mutex");
+
+	semaphore = CreateSemaphore(nullptr, MIN_SEMAPHORE_VALUE, MAX_SEMAPHORE_VALUE, L"semaphore");
+
+	eventReaderA = CreateEvent(nullptr, FALSE, FALSE, L"readerA");
+	eventReaderB = CreateEvent(nullptr, FALSE, FALSE, L"readerB");
+	eventReaderC = CreateEvent(nullptr, FALSE, FALSE, L"readerC");
+
+	eventsWriter = new HANDLE[ARRAY_SIZE];
+	eventsWriter[A_INDEX] = CreateEvent(nullptr, FALSE, FALSE, L"writerA");
+	eventsWriter[B_INDEX] = CreateEvent(nullptr, FALSE, FALSE, L"writerB");
+	eventsWriter[C_INDEX] = CreateEvent(nullptr, FALSE, FALSE, L"writerC");
+
+	eventWriterEnded = CreateEvent(nullptr, FALSE, FALSE, L"writerEnded");
+	eventReaderEnded = CreateEvent(nullptr, FALSE, FALSE, L"readerEnded");
+}
+
+void initSourceData(int& n, int& m) {
+	do {
+		std::cout << "Administrator: Enter number of processes: ";
+		std::cin >> n;
+	} while (n < MIN_NUMBER_OF_PROCESSES
+		&& std::cout << "Administrator: Number of processes must be positive\n");
+	do {
+		std::cout << "Administrator: Enter number of sended messages for Writer (and recieved for Reader): ";
+		std::cin >> m;
+	} while (m < MIN_NUMBER_OF_MESSAGES
+		&& std::cout << "Administrator: number of sended messages for Writer (and recieved for Reader) must be positive\n");
+
+	siW = new STARTUPINFO[n];
+	siR = new STARTUPINFO[n];
+	piW = new PROCESS_INFORMATION[n];
+	piR = new PROCESS_INFORMATION[n];
+}
+
+wchar_t* buildCommandLine(std::wstring str, int m) {
+	std::wstring line = str;
+	line += std::to_wstring(m) + L" ";
+
+	wchar_t* cmd = new wchar_t[line.size()];
+	wcscpy_s(cmd, line.size() + OFFSET, line.c_str());
+
+	return cmd;
+}
+
+int runAllProcesses(int n, wchar_t* cmd1, wchar_t* cmd2) {
+	for (int i = 0; i < n; i++) {
+		ZeroMemory(&siW[i], sizeof(STARTUPINFO));
+		ZeroMemory(&siR[i], sizeof(STARTUPINFO));
+		if (!CreateProcess(nullptr, cmd1, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &siW[i], &piW[i])) {
+			std::cout << "Administrator: Writer opening error";
+			return GetLastError();
+		}
+		if (!CreateProcess(nullptr, cmd2, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &siR[i], &piR[i])) {
+			std::cout << "Administrator: Reader opening error";
+			return GetLastError();
+		}
+	}
+
+	return 0;
+}
+
+void startMessageTranslation(int n, int m) {
+	int k;
+	for (int i = 0; i < n * m; i++) {
+		k = WaitForMultipleObjects(ARRAY_SIZE, eventsWriter, FALSE, INFINITE);
+		k -= WAIT_OBJECT_0;
+		if (k == A_INDEX) {
+			std::cout << "Administrator: A\n";
+			SetEvent(eventReaderA);
+		} else if (k == B_INDEX) {
+			std::cout << "Administrator: B\n";
+			SetEvent(eventReaderB);
+		} else {
+			std::cout << "Administrator: C\n";
+			SetEvent(eventReaderC);
+		}
+		if ((i + OFFSET) % m == 0) {
+			WaitForSingleObject(eventWriterEnded, INFINITE);
+			std::cout << "Administrator: Writer ended\n";
+			WaitForSingleObject(eventReaderEnded, INFINITE);
+			std::cout << "Administrator: Reader ended\n";
+		}
+	}
+}
+
+void releaseResources(wchar_t* cmd1, wchar_t* cmd2) {
+	CloseHandle(mutex);
+	CloseHandle(semaphore);
+	CloseHandle(eventReaderA);
+	CloseHandle(eventReaderB);
+	CloseHandle(eventReaderC);
+	CloseHandle(eventsWriter[A_INDEX]);
+	CloseHandle(eventsWriter[B_INDEX]);
+	CloseHandle(eventsWriter[C_INDEX]);
+	CloseHandle(eventWriterEnded);
+	CloseHandle(eventReaderEnded);
+
+	delete[] siW, siR, piW, piR, cmd1, cmd2, eventsWriter;
+	siW = siR = nullptr;
+	piW = piR = nullptr;
+	cmd1 = cmd2 = nullptr;
+	eventsWriter = nullptr;
+}
+
+int main() {
+
+	createSynchObj();
 
 	int n, m;
-	cout << "Administrator: Enter number of processes: ";
-	cin >> n;
-	cout << "Administrator: Enter number of sended messages for Writer (and recieved for Reader): ";
-	cin >> m;
+	initSourceData(n, m);
 
-	STARTUPINFO* si_w = new STARTUPINFO[n];
-	STARTUPINFO* si_r = new STARTUPINFO[n];
-	PROCESS_INFORMATION* pi_w = new PROCESS_INFORMATION[n];
-	PROCESS_INFORMATION* pi_r = new PROCESS_INFORMATION[n];
+	wchar_t* cmdW = buildCommandLine(L"\"Writer.exe\" ", m);
+	wchar_t* cmdR = buildCommandLine(L"\"Reader.exe\" ", m);
 
-	wstring str_w = L"\"Writer.exe\" ";
-	str_w += to_wstring(m);
-	wchar_t* wstr_w = new wchar_t[str_w.length()];
-	wcscpy_s(wstr_w, str_w.length() + 1, str_w.c_str());
-
-	wstring str_r = L"\"Reader.exe\" ";
-	str_r += to_wstring(m);
-	wchar_t* wstr_r = new wchar_t[str_r.length()];
-	wcscpy_s(wstr_r, str_r.length() + 1, str_r.c_str());
-
-	for (int i = 0; i < n; i++)
-	{
-		ZeroMemory(&si_w[i], sizeof(STARTUPINFO));
-		ZeroMemory(&si_r[i], sizeof(STARTUPINFO));
-		if (!CreateProcess(nullptr, wstr_w, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si_w[i], &pi_w[i]))
-		{
-			cout << "Administrator: Writer opening error";
-			return GetLastError();
-		}
-		if (!CreateProcess(nullptr, wstr_r, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si_r[i], &pi_r[i]))
-		{
-			cout << "Administrator: Reader opening error";
-			return GetLastError();
-		}
+	int flag = runAllProcesses(n, cmdW, cmdR);
+	if (flag != 0) {
+		return flag;
 	}
 
-	for (int i = 0; i < n * m; i++)
-	{
-		int k = WaitForMultipleObjects(3, Events_Writer, FALSE, INFINITE);
-		k -= WAIT_OBJECT_0;
-		if (k == 0)
-		{
-			cout << "Administrator: A\n";
-			SetEvent(Event_Reader_A);
-		}
-		else if (k == 1)
-		{
-			cout << "Administrator: B\n";
-			SetEvent(Event_Reader_B);
-		}
-		else
-		{
-			cout << "Administrator: C\n";
-			SetEvent(Event_Reader_C);
-		}
-		if ((i + 1) % m == 0)
-		{
-			WaitForSingleObject(Event_Writer_Ended, INFINITE);
-			cout << "Administrator: Writer ended\n";
-			WaitForSingleObject(Event_Reader_Ended, INFINITE);
-			cout << "Administrator: Reader ended\n";
-		}
-	}
+	startMessageTranslation(n, m);
 
-	CloseHandle(Mutex);
-	CloseHandle(Semaphore);
-	CloseHandle(Event_Reader_A);
-	CloseHandle(Event_Reader_B);
-	CloseHandle(Event_Reader_C);
-	CloseHandle(Events_Writer[0]);
-	CloseHandle(Events_Writer[1]);
-	CloseHandle(Events_Writer[2]);
-	CloseHandle(Event_Writer_Ended);
-	CloseHandle(Event_Reader_Ended);
-
-	delete[] si_w, si_r, pi_w, pi_r, str_w, str_r, Events_Writer;
-	si_w = si_r = nullptr;
-	pi_w = pi_r = nullptr;
-	Events_Writer = nullptr;
+	releaseResources(cmdW, cmdR);
 
 	return 0;
 }
